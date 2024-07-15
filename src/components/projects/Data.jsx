0b14e2789345c1bf342fbe553ad428818414a123
Rob <52356169+R0B3R799@users.noreply.github.com>
2024-07-15 14:57:39 +0100

    Add files via upload

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -10,35 +10,35 @@ export const projectsData = [
         image: Project7,
         title: "Panda Robotics Ltd",
         categories: ["Web Automation", "Frontend"], // Updated to support multiple categories
-        description: "Panda Robotics Ltd specializes in developing advanced robotics solutions for various industries. Our projects include automation of repetitive tasks, enhancing productivity, and reducing operational costs.",
+        description: "A Desktop python application automating the process of obtaining limited edition products.",
     },
     {
         id: 5,
         image: Project5,
         title: "Automating AI Content Creation",
         categories: ["Web Automation"],
-        description: "This project focuses on automating the creation of AI-generated content for various platforms. It includes developing algorithms and tools to streamline content production, ensuring quality and consistency.",
+        description: "This project focuses on automating the creation of AI-generated content for shorts platforms. It includes using AI video generation, AI voice overs and automatically adding subtitles to content.",
     },
     {
         id: 1,
         image: Project1,
         title: "NFT Marketplace Monitor",
         categories: ["Blockchain"],
-        description: "The NFT Marketplace Monitor provides real-time tracking of NFT prices and transactions across multiple platforms. It helps users stay updated with the latest market trends and make informed investment decisions.",
+        description: "The NFT Marketplace Monitor provided real-time tracking of NFT prices posting deals to users of assets that were underpriced.",
     },
     {
         id: 2,
         image: Project2,
         title: "Ethereum NFT Profit Calculations",
         categories: ["Blockchain"],
-        description: "This tool calculates potential profits from Ethereum NFT investments by analyzing historical data and market trends. It assists investors in making data-driven decisions.",
+        description: "This tool calculates potential profits from Ethereum NFT investments by collecting a users on chain transactions and calcualting profits taking account of gas fees and sale taxes.",
     },
     {
         id: 3,
         image: Project3,
         title: "Exploration of the ERC-6551 Standard",
         categories: ["Blockchain"],
-        description: "This project explores the ERC-6551 standard, focusing on its implementation and potential applications in the blockchain ecosystem. It aims to provide insights into the benefits and challenges of this standard.",
+        description: "We create a decentralised platform where cars are tokenised on the blockchain. This tokenisation not only facilitates the secure and transparent transfer of ownership but also allows for modular additions to the system for various car-related attributes and certifications such as MOT certificates and parking fines into one unified system.  Our approach ensures that all relevant data about a vehicle, from MOT history to Manufacturer, is immutable and easily accessible, reducing the potential for fraud and errors in record-keeping. The results of this work demonstrate that the ERC-6551 standard not only offers a viable alternative to traditional methods of asset management but also enhances the functionality, paving the way for a decentralised future.",
     },
 ];
 
