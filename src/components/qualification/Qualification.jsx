29319861720208c9b54e06b1b25eff11601bc6ac
Rob <52356169+R0B3R799@users.noreply.github.com>
2025-11-06 15:55:08 +0000

    Add files via upload

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -79,8 +79,8 @@ const Qualification = () => {
                     : "qualification__content"}>
                     <div className="qualification__data">
                         <div>
-                            <h3 className="qualification__title">CEO Panda Robotics</h3>
-                            <span className="qualification__subtitle">Web Automation</span>
+                            <h3 className="qualification__title">Panda Robotics LTD</h3>
+                            <span className="qualification__subtitle">Founder & Lead Developer - Web Automation SaaS</span>
                             <div className="qualification__calendar">
                                 <HiOutlineCalendar className="qualification__calendar-icon" />
                                 2020-2022
@@ -98,21 +98,21 @@ const Qualification = () => {
                             <span className="qualification__line"></span>
                         </div>
                         <div>
-                            <h3 className="qualification__title">Blockchain Development</h3>
-                            <span className="qualification__subtitle">Tools and Monitors</span>
+                            <h3 className="qualification__title">Blockchain Profit Calculator</h3>
+                            <span className="qualification__subtitle">Trading Tool - Python, Blockchain APIs</span>
                             <div className="qualification__calendar">
                                 <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2021-2023
+                                2022
                             </div>
                         </div>
                     </div>
                     <div className="qualification__data">
                         <div>
-                            <h3 className="qualification__title">UX Designer</h3>
-                            <span className="qualification__subtitle">Freelance - Remote</span>
+                            <h3 className="qualification__title">Digital Asset Price Monitoring Tool</h3>
+                            <span className="qualification__subtitle">Trading Software - Python, Web APIs</span>
                             <div className="qualification__calendar">
                                 <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2024-Present
+                                2022
                             </div>
                         </div>
                         <div>
@@ -127,11 +127,69 @@ const Qualification = () => {
                             <span className="qualification__line"></span>
                         </div>
                         <div>
-                            <h3 className="qualification__title">Web Automation</h3>
-                            <span className="qualification__subtitle">Freelance - Remote</span>
+                            <h3 className="qualification__title">Restaurant Review Mobile App + Social Media Web Application</h3>
+                            <span className="qualification__subtitle">Android Development + Full-Stack Development - Kotlin, Laravel, PHP, Firebase, Google APIs</span>
                             <div className="qualification__calendar">
                                 <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2024-Present
+                                2024
+                            </div>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">AI Content Automation Platform</h3>
+                            <span className="qualification__subtitle">Content Generation - Python, Computer Vision, NLP</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2024
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">Front Office Staff</h3>
+                            <span className="qualification__subtitle">Higashiyama Niseko Village, Ritz Carlton, Japan</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2024
+                            </div>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">F&B Service Staff</h3>
+                            <span className="qualification__subtitle">Nikko Style Hanazono, Niseko, Japan</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2024-2025
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+                    <div className="qualification__data">
+                        <div></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">Working Holiday Work Platform</h3>
+                            <span className="qualification__subtitle">Full-Stack Web Application - React, TypeScript, AWS</span>
+                            <div className="qualification__calendar">
+                                <HiOutlineCalendar className="qualification__calendar-icon" />
+                                2025
                             </div>
                         </div>
                     </div>
